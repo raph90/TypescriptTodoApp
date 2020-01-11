99288238c6b0e07cc47b9bf2b0b8b65a444abd95
Raphael Hetherington <raph@Raphaels-MacBook-Pro.local>
2020-01-11 16:28:07 +0000

    multiple additions to the side bar

--- client/src/js/index.ts
+++ client/src/js/index.ts
@@ -1,6 +1,19 @@
 import { hello } from "./views/test";
 import "../css/main.scss";
-
+import '@fortawesome/fontawesome-free/js/all'
+import axios, { AxiosResponse } from "axios"
 const hi = hello("Agatha");
 
-console.log(hi)
+const localState = {
+    data: Object
+}
+
+async function getData(): Promise<any> {
+    const data = await axios.get('/api/hello');
+    localState.data = data.data
+    console.log(localState)
+}
+
+getData();
+
+
